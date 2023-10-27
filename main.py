5977ecb51aeca1d705004f805196c2c0923833cb
Aman Mohammad <aman.mohammed@education.gov.uk>
2023-10-27 09:22:35 +0100

    python based game

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import pygame
+
+pygame.init()
+root = Tk()
+root.title("Who Wants To Be A Millionaire?")
+root.geometry('132x652+0+0')
+root.configure(background = 'black')
+
+ABC = Frame(root, bg = 'black')
+ABC.grid()
+
+ABC1 = Frame(root, bg = 'black', bd = 20, width = 900, height = 600)
+ABC1.grid(row = 0, column = 0)
+ABC2 = Frame(root, bg = 'black', bd = 20, width = 452, height = 600)
+ABC2.grid(row = 0, column = 1)
+
+ABC1a = Frame(ABC1, bg = 'green', bd = 20, width = 900, height = 200)
+ABC1a.grid(row = 0, column = 0)
+ABC1b = Frame(ABC1, bg = 'pink', bd = 20, width = 900, height = 200)
+ABC1b.grid(row = 1, column = 0)
+ABC1c = Frame(ABC1, bg = 'gray', bd = 20, width = 900, height = 200)
+ABC1c.grid(row = 2, column = 0)
