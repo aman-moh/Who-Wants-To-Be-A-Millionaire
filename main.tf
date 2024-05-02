d8cf3b1fff840d7d2d2c3dd7cc8e9296c3872d77
Aman Mohammad <amanmohammad741@gmail.com>
2024-05-02 16:06:08 +0100

    format main.tf file

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "azurerm" {
-    features{}  
+  features {}
 }
 
 resource "azurerm_resource_group" "wwtbamrg" {
-    name = "wwtbamrg"
-    location = "West Europe"
+  name     = "wwtbamrg"
+  location = "West Europe"
 }
 
 resource "azurerm_storage_account" "wwtbamstr" {
-  name = "wwtbamstr"
-  resource_group_name = azurerm_resource_group.wwtbamrg.name
-  location = "UK West"
-  account_tier = "Standard"
-  account_replication_type = "LRS"
+  name                      = "wwtbamstr"
+  resource_group_name       = azurerm_resource_group.wwtbamrg.name
+  location                  = "UK West"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
   enable_https_traffic_only = true
 
   static_website {
@@ -21,17 +21,17 @@ resource "azurerm_storage_account" "wwtbamstr" {
 }
 
 resource "azurerm_storage_blob" "wwtbamstrblob" {
-    name = "index.html"
-    storage_account_name = azurerm_storage_account.wwtbamstr.name
-    storage_container_name = "$web"
-    type = "Block"
-    content_type = "text/html"
-    source = "index.html"
-    }
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.wwtbamstr.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
+}
 
 resource "azurerm_static_web_app" "wwtbamweb" {
-    name = "wwtbamweb"
-    location = azurerm_resource_group.wwtbamrg.location
-    resource_group_name = azurerm_resource_group.wwtbamrg.name
-    sku_tier = "Free"
+  name                = "wwtbamweb"
+  location            = azurerm_resource_group.wwtbamrg.location
+  resource_group_name = azurerm_resource_group.wwtbamrg.name
+  sku_tier            = "Free"
 }
